ccad13165b88b8cfba178cea1fd171157f8fdcd8
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-03-31 17:16:47 -0500

    :wrench: Vite ports are established

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    port: 11451,
+    strictPort: true
+  },
+  server: {
+    port: 11450,
+    strictPort: true
+  },
   resolve: {
     alias: {
       '@': '/src'
