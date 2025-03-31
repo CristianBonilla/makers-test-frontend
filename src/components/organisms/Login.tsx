c77ee15e4f02b3e1e9e3d2b0fabe97f3c7646d7a
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-03-31 17:16:47 -0500

    :lipstick: Design and layout of login and register components has been completed

--- src/components/organisms/Login.tsx
+++ src/components/organisms/Login.tsx
@@ -1,10 +1,28 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   return (
     <Container className='login'>
       <div className='login__wrap'>
         <h2 className='text-secondary text-start'>Iniciar Sesión</h2>
+        <Form noValidate autoComplete='off' spellCheck='false'>
+          <Form.Group className='mb-3' controlId='username-or-email'>
+            <Form.Label>Usuario o Email</Form.Label>
+            <Form.Control type='text' placeholder='Usuario o Email' />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='password'>
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type='password' placeholder='Contraseña' />
+          </Form.Group>
+          <div className='d-flex align-items-start align-items-sm-center flex-column flex-sm-row justify-content-sm-between w-100'>
+            <a id='forgot-password' className='forgot-password mb-2 mb-sm-0' href="" onClick={event => event.preventDefault()}>
+              Olvidaste la contraseña?
+            </a>
+            <Button type='submit' variant='primary' size='lg'>Iniciar Sesión</Button>
+          </div>
+        </Form>
       </div>
     </Container>
   );
--- src/components/organisms/Register.tsx
+++ src/components/organisms/Register.tsx
@@ -1,10 +1,43 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 function Register() {
   return (
     <Container className='register'>
       <div className="register__wrap">
         <h2 className='text-secondary text-start'>Registrarse</h2>
+        <Form noValidate autoComplete='off' spellCheck='false'>
+          <Form.Group className='mb-3' controlId='identification-document'>
+            <Form.Label>Documento de identificación</Form.Label>
+            <Form.Control type='text' placeholder='Documento de identificación'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='username'>
+            <Form.Label>Nombre de usuario</Form.Label>
+            <Form.Control type='text' placeholder='Nombre de usuario'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='email'>
+            <Form.Label>Correo</Form.Label>
+            <Form.Control type='text' placeholder='Correo electrónico'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='password'>
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type='password' placeholder='Contraseña'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='confirm-password'>
+            <Form.Label>Confirmar contraseña</Form.Label>
+            <Form.Control type='password' placeholder='Confirmar contraseña'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='firstname'>
+            <Form.Label>Nombres</Form.Label>
+            <Form.Control type='text' placeholder='Nombres'/>
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='lastname'>
+            <Form.Label>Apellidos</Form.Label>
+            <Form.Control type='text' placeholder='Apellidos'/>
+          </Form.Group>
+          <Button type='submit' variant='primary' size='lg'>Registrarse</Button>
+        </Form>
       </div>
     </Container>
   );
